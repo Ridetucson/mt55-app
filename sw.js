d7b8b4180b8cc93cb08230e8f7931e69fa7f5854
Ridetucson <enjoytherideaz1@gmail.com>
2025-09-08 15:04:59 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,25 @@
-const CACHE='mtb55-v2';
-const ASSETS=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
+const CACHE = 'rt-reparaciones-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-72.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=> c.addAll(ASSETS)));
+});
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=> Promise.all(keys.filter(k=>k!==CACHE).map(k=> caches.delete(k)))));
+});
+self.addEventListener('fetch', e=>{
+  const req = e.request;
+  e.respondWith(
+    caches.match(req).then(res=> res || fetch(req).then(r=>{
+      const copy = r.clone();
+      caches.open(CACHE).then(c=> c.put(req, copy));
+      return r;
+    }).catch(()=> caches.match('./index.html')))
+  );
+});
